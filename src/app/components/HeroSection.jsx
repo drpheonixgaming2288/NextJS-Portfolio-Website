00f5bdb8c450b94639c5b3bc3793f6d992861461
unknown <drpheonixgaming2288@gmail.com>
2024-05-26 21:42:07 -0500

    Everything

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,92 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Dylan",
+                1000,
+                "a Web Developer",
+                1000,
+                "a Full Stack Developer",
+                1000,
+                "a System Administrator",
+                1000,
+                "an Infrastructure Engineer",
+                1000,
+                "a Linux Administrator",
+                1000,
+                "a Game Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            With a passion for technology and a diverse skill set, I bring
+            innovative solutions to every project. From developing user-friendly
+            web applications to managing complex IT infrastructures, I am
+            dedicated to delivering high-quality results. Let's create something
+            amazing together!
+          </p>
+          <div>
+            <Link
+              href="https://docs.google.com/document/d/1KX4nw4Og-jYBGSe6tEIq-aEDvlZa2r75WWIu4-7lBjI/export?format=pdf"
+              target="_blank"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Download CV
+            </Link>
+            {/* <Link
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link> */}
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className="absolute rounded-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              style={{border: '0.3rem solid black'}}
+              width={500}
+              height={500}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
